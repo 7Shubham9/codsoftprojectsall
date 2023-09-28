794e39986ff957b871a4bbd7abd1a3a4b448f561
Shubham Solanki <129917365+7Shubham9@users.noreply.github.com>
2023-09-28 20:07:11 +0530

    Add files via upload

---
+++ simplecalc.cpp
@@ -0,0 +1,45 @@
+// simple calculator 
+// + -* /
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+	char op;
+	float num1, num2;
+
+
+	cin >> op;
+
+	
+	cin >> num1 >> num2;
+
+	
+	switch (op) {
+	
+	case '+':
+		cout << num1 + num2;
+		break;
+
+	
+	case '-':
+		cout << num1 - num2;
+		break;
+	case '*':
+		cout << num1 * num2;
+		break;
+
+
+	case '/':
+		cout << num1 / num2;
+		break;
+
+	
+	default:
+		cout << "Error! operator is not correct";
+	}
+	
+
+	return 0;
+}
